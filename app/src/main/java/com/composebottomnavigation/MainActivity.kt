f088b7386e7743d5da070a72cde7caf1ba0cc096
Viacheslav Panasiuk <badgerle82@gmail.com>
2022-11-29 16:56:21 +0200

    small fixes in BottomNavMenu

--- app/src/main/java/com/composebottomnavigation/MainActivity.kt
+++ app/src/main/java/com/composebottomnavigation/MainActivity.kt
@@ -3,6 +3,15 @@ package com.composebottomnavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.navigation.compose.rememberNavController
+import com.composebottomnavigation.navigation.Navigation
+import com.composebottomnavigation.screens.BottomNavigationMenu
+import com.composebottomnavigation.screens.model.BottomNavItem
 import com.composebottomnavigation.ui.theme.ComposeBottomNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +19,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBottomNavigationTheme {
-
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationMenu(
+                            items = listOf(
+                                BottomNavItem(
+                                    name = "Home",
+                                    route = "home",
+                                    icon = Icons.Default.Home
+                                ),
+                                BottomNavItem(
+                                    name = "Chat",
+                                    route = "chat",
+                                    icon = Icons.Default.Notifications,
+                                    badgeCount = 54
+                                ),
+                                BottomNavItem(
+                                    name = "Settings",
+                                    route = "settings",
+                                    icon = Icons.Default.Settings,
+                                    badgeCount = 3
+                                )
+                            ),
+                            navController = navController,
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                ) {
+                    Navigation(navController = navController)
+                }
             }
         }
     }
--- app/src/main/java/com/composebottomnavigation/screens/BottomNavMenu.kt
+++ app/src/main/java/com/composebottomnavigation/screens/BottomNavMenu.kt
@@ -1,11 +1,64 @@
 package com.composebottomnavigation.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.composebottomnavigation.screens.model.BottomNavItem
 
 @Composable
 fun BottomNavigationMenu(
-    title: String,
-
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
 ) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
 
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.DarkGray,
+        elevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.Green,
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    BottomNavMenuItem(item = item, isSelected = selected)
+                }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun BottomNavMenuItem(item: BottomNavItem, isSelected: Boolean) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        if (item.badgeCount > 0) {
+            BadgeBox(
+                badgeContent = {
+                    Text(text = item.badgeCount.toString())
+                }
+            ) {
+                Icon(imageVector = item.icon, contentDescription = item.name)
+            }
+        } else {
+            Icon(imageVector = item.icon, contentDescription = item.name)
+        }
+        if (isSelected) {
+            Text(text = item.name, textAlign = TextAlign.Center, fontSize = 10.sp)
+        }
+    }
 }
